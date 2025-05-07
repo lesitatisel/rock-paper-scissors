9a5105c5d440f2872658d97889f8a900dc6780db
lesi <lesitatisel@gmail.com>
2025-05-07 22:48:33 +0500

    Create getComputerChoice() function
    getComputerChoice() functions gets a random number
    from 1 to 3 (each corresponding to 'rock', 'paper'
    and 'scissor' respectively) and returns the corresponding
    string.

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
 // 2. Plan
 
 // 3. Pseudocode
+
+
+
+const getComputerChoice = function() {
+  const choice = Math.floor(Math.random() * (4 - 1) + 1);
+  
+  if (choice == 1) {
+    return "rock";
+  } else if (choice == 2) {
+    return "paper";
+  } else {
+    return "scissors"
+  }
+
+}
+
+console.log(getComputerChoice());
